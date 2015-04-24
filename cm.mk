9fcfa4c44739092502cdf1f5e4833f2de44faf2a
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2015-04-24 18:54:15 +0200

    update signature
    Change-Id: I21b19c839d22216fe9e1e80f7172b71a8f77d203

--- cm.mk
+++ cm.mk
@@ -7,8 +7,8 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 $(call inherit-product, device/sony/scorpion_windy/full_scorpion_windy.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP611
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/SGP611/SGP611:4.4.4/23.0.1.A.0.167/WP_3Rw:user/release-keys
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="SGP611-user 4.4.4 23.0.1.A.0.167 WP_3Rw release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/SGP611/SGP611:5.0.2/23.1.A.0.690/237136704:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="SGP611-user 5.0.2 23.1.A.0.690 237136704 release-keys"
 
 PRODUCT_NAME := cm_scorpion_windy
 PRODUCT_DEVICE := scorpion_windy
