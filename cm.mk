ba0bd360d62e19ee59cb73dd2d381029bd33c084
David Viteri <davidteri91@gmail.com>
2015-08-04 20:29:13 +0200

    scorpion_windy: update fingerprint to latest lollipop release
    Change-Id: Ia24c1c0f67abcf7906d39f84a70ecc45d9cfd679
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- cm.mk
+++ cm.mk
@@ -7,8 +7,8 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 $(call inherit-product, device/sony/scorpion_windy/full_scorpion_windy.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP611
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/SGP611/SGP611:5.0.2/23.1.A.0.690/237136704:user/release-keys
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="SGP611-user 5.0.2 23.1.A.0.690 237136704 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/SGP611/SGP611:5.1.1/23.4.A.0.546/1598523878:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="SGP611-user 5.1.1 23.4.A.0.546 1598523878 release-keys"
 
 PRODUCT_NAME := cm_scorpion_windy
 PRODUCT_DEVICE := scorpion_windy
