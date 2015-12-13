cd1d7c1bf2196e09755cfa04d4ffe50960664837
Chirayu Desai <chirayudesai1@gmail.com>
2015-12-13 09:18:40 +0530

    Update defconfig name for windy and lte unification
    * The same 3.10 kernel binary can be used on windy and lte both.
    
    Change-Id: Ic8b731cfb4e91d8e0f52c3d90368085b6b06fadc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,6 +17,3 @@ include device/sony/scorpion_windy/BoardConfigCommon.mk
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := SGP611,SGP612,scorpion_windy
-
-# Kernel properties
-TARGET_KERNEL_CONFIG := cm_shinano_scorpion_windy_defconfig
--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -22,6 +22,9 @@ BOARD_HARDWARE_CLASS += device/sony/scorpion_windy/cmhw
 # For two-stage boot configuration
 TARGET_RECOVERY_DEVICE_DIRS := device/sony/scorpion_windy
 
+# Kernel properties
+TARGET_KERNEL_CONFIG := cm_shinano_scorpion_defconfig
+
 # Partition information
 BOARD_VOLD_MAX_PARTITIONS := 26
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 12253589504 # 12253605888 - 16384
