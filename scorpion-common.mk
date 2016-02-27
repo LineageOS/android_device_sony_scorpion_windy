cdd46709fd9636ba048d14550518338b4657491e
Fábio Silva <fabio.fernando.osilva@gmail.com>
2016-02-27 12:53:16 -0300

    scorpion_windy: move touchscreen *.idc to device tree
    Change-Id: Ic64cb7bea6c8299fd6df1a26fe27bef6aa175fd0

--- scorpion-common.mk
+++ scorpion-common.mk
@@ -40,6 +40,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
    $(LOCAL_PATH)/thermanager.xml:system/etc/thermanager.xml
 
+# Touchscreen
+PRODUCT_COPY_FILES += \
+   $(LOCAL_PATH)/clearpad.idc:system/usr/idc/clearpad.idc
+
 # call dalvik heap config
 $(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk)
 
